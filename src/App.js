bcd1aee640d156bdcf1a9ff31df915e57e0719a2
Aaron Lei <aaron.l@live.ca>
2021-05-21 13:25:37 -0400

    added comments to app.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import LandingPage from "./components/LandingPage/LandingPage";
 import NavBar from "./components/NavBar/NavBar";
 import Form from "./components/Form/Form";
 
+{
+  /* All components are called on the main App.js file,  */
+}
+
+{
+  /*This method of programming keep the code very clean.*/
+}
+{
+  /* Also helps with website optimization because indiviual components can refresh when the specific components  */
+}
 function App() {
   return (
     <div className="App">
       <NavBar />
+      {/* The usage of props in React helps with editing the webpage with ease. */}
       <LandingPage
         title="Solve payroll for your small business clients."
         paragraph="Wagepoint also helps you generate consistent revenue for your accounting or bookkeeping firm with simple payroll software that's easy to use and recommend."
